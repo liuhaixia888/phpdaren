5e8e922bdc5bd0cf830e7b0ef679e818090cc34b
liuhaixia888 <haixialiu@haixialiudeiMac.local>
2015-07-15 16:37:26 +0800

    提交静态页面

---
+++ html/src/fis-conf.js
@@ -0,0 +1,20 @@
+//file: fis-conf.js
+
+//后缀名的less的文件使用fis-parser-less编译
+//modules.parser.less表示设置后缀名为less的文件的parser，第二个less表示使用fis-parser-less进行编译
+fis.config.set('modules.parser.less', 'less');
+//将less文件编译为css
+fis.config.set('roadmap.ext.less', 'css');
+
+fis.config.merge({
+	pack:{
+		'css/home.css': ['**.less']
+	}
+});
+
+fis.config.set('roadmap.path',[
+    {
+    	reg : "**.html",
+        useStandard: false
+    }
+]);
